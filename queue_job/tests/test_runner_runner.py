daeff76edc721abcf842e22bd88dc116687055bd
Pierre Verkest <pierreverkest84@gmail.com>
2025-03-14 00:18:38 +0100

    [FIX] queue_job: job runner open pipe that are never closed properly
    this make a lot of open file descriptors that we got the limit while instentiate
    jobrunner from queue_job_cron_jobrunner in the current channel implementation
    https://github.com/OCA/queue/pull/750

--- queue_job/tests/test_runner_runner.py
+++ queue_job/tests/test_runner_runner.py
@@ -3,8 +3,32 @@
 
 # pylint: disable=odoo-addons-relative-import
 # we are testing, we want to test as we were an external consumer of the API
+import os
+from unittest import TestCase
+
 from odoo.addons.queue_job.jobrunner import runner
 
 from .common import load_doctests
 
 load_tests = load_doctests(runner)
+
+
+class TestRunner(TestCase):
+    def test_runner_file_descriptor(self):
+        def is_fd_open(fd):
+            try:
+                os.fstat(fd)  # Vérifie si le fd est valide
+                return True  # Pas d'erreur => le fd est ouvert
+            except OSError:
+                return False  # Erreur => le fd est fermé
+
+        a_runner = runner.QueueJobRunner.from_environ_or_config()
+
+        read_fd, write_fd = a_runner._stop_pipe
+        self.assertTrue(is_fd_open(read_fd))
+        self.assertTrue(is_fd_open(write_fd))
+
+        del a_runner
+
+        self.assertFalse(is_fd_open(read_fd))
+        self.assertFalse(is_fd_open(write_fd))
